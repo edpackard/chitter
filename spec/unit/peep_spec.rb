027d9cf5928024a9dae7b97f3709d49e2af2415c
edpackard <edpackard81@gmail.com>
2021-10-26 10:32:54 +0100

    attempt at unit test for peep class

---
+++ spec/unit/peep_spec.rb
@@ -0,0 +1,21 @@
+require 'peep'
+
+describe Peep do
+
+  let(:connection) { double(:connection) }
+  result = [ 
+    {'content' => 'Test Peep 1'}, 
+    {'content' => 'Test Peep 2'}, 
+    {'content' => 'Test Peep 3'}
+  ]
+
+  it 'returns a list of bookmarks' do
+    allow(PG).to receive(:connect).and_return(connection)
+    allow(connection).to receive(:exec).and_return(result)
+    peeps = Peep.all
+    expect(peeps).to include 'Test Peep 1'
+    expect(peeps).to include 'Test Peep 2'
+    expect(peeps).to include 'Test Peep 3'
+  end
+
+end
