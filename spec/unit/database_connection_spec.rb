85df7c0c6f97489eea449fdaecf18cb5f96e56e1
edpackard <edpackard81@gmail.com>
2021-10-28 09:10:44 +0100

    Add DatabaseConnection connection method

--- spec/unit/database_connection_spec.rb
+++ spec/unit/database_connection_spec.rb
@@ -2,10 +2,20 @@ require 'database_connection'
 
 describe DatabaseConnection do
 
+  let(:connection) { double(:connection) }
+
   describe '.setup' do
     it 'sets up connection to database (via PG)', :no_database do
       expect(PG).to receive(:connect).with(dbname: 'chitter_test')
       DatabaseConnection.setup('chitter_test')
+    end
+  end
+
+  describe '.connection' do
+    it 'maintains a persistent connection', :no_database do
+      allow(PG).to receive(:connect).with(dbname: 'chitter_test').and_return(connection)
+      DatabaseConnection.setup('chitter_test')
+      expect(DatabaseConnection.connection).to eq connection
     end
   end
 
