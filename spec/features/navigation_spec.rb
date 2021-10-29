71416f9ca858eda645c4b5d608bcc671facfc473
edpackard <edpackard81@gmail.com>
2021-10-29 16:28:10 +0100

    add navigation from peeps/new to peeps/

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -3,5 +3,14 @@ feature 'navigation' do
     visit('/peeps')
     click_button('New Peep')
     expect(page).to have_current_path('/peeps/new')
+    expect(page).to have_no_button 'New Peep'
   end
+
+  scenario 'A user can click on a button on new post page to go to index' do
+    visit('/peeps/new')
+    click_button('See All Peeps')
+    expect(page).to have_current_path('/peeps')
+    expect(page).to have_no_button 'See All Peeps'
+  end
+
 end
