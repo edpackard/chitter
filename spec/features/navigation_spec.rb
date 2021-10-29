0e34e96c2774f619e97ac4b04c0763880020f768
edpackard <edpackard81@gmail.com>
2021-10-29 18:05:52 +0100

    update css and navigation/input elements

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -1,16 +1,16 @@
 feature 'navigation' do
   scenario 'A user can click on a button in index to go to new post page' do
     visit('/peeps')
-    click_button('New Peep')
+    click_link('New Peep')
     expect(page).to have_current_path('/peeps/new')
-    expect(page).to have_no_button 'New Peep'
+    expect(page).to have_no_link 'New Peep'
   end
 
   scenario 'A user can click on a button on new post page to go to index' do
     visit('/peeps/new')
-    click_button('See All Peeps')
+    click_link('See All Peeps')
     expect(page).to have_current_path('/peeps')
-    expect(page).to have_no_button 'See All Peeps'
+    expect(page).to have_no_link 'See All Peeps'
   end
 
 end
--- spec/features/new_peep_spec.rb
+++ spec/features/new_peep_spec.rb
@@ -3,7 +3,7 @@ feature 'Posting a new peep' do
     visit('/peeps/new')
     expect(page).to have_field('content', placeholder: 'Write your peep here!')
     fill_in('content', with: 'A brand new peep!')
-    click_button('Submit')
+    click_button('Peep!')
     expect(page).to have_current_path('/peeps')
     expect(page).to have_content('A brand new peep!')
   end
