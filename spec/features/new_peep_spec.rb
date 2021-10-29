dbef3d892c8d51d6b3e3e5f1826bed9cc4757b02
edpackard <edpackard81@gmail.com>
2021-10-29 17:12:48 +0100

    Add basic CSS

--- spec/features/new_peep_spec.rb
+++ spec/features/new_peep_spec.rb
@@ -1,6 +1,7 @@
 feature 'Posting a new peep' do
   scenario 'A user can post a peep to Chitter' do
     visit('/peeps/new')
+    expect(page).to have_field('content', placeholder: 'Write your peep here!')
     fill_in('content', with: 'A brand new peep!')
     click_button('Submit')
     expect(page).to have_current_path('/peeps')
