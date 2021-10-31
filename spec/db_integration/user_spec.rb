cc61396e43a44f6058e11cfa3d1b226295d42f88
edpackard <edpackard81@gmail.com>
2021-10-31 09:32:40 +0000

    rubocop linting

--- spec/db_integration/user_spec.rb
+++ spec/db_integration/user_spec.rb
@@ -1,21 +1,24 @@
 require 'database_helpers'
 
-describe '.create' do
-  it 'creates new user' do
-    user = User.create(name: 'TestUser21', password: 'drowssap', email: 'test@example.com')
-    persisted_data = persisted_data(table: :users, id: user.id)
-    expect(user.id).to eq persisted_data['id']
-    expect(user.name).to eq('TestUser21')
-    expect(user.email).to eq 'test@example.com'
+describe User do
+
+  describe '.create' do
+    it 'creates new user' do
+      user = User.create(name: 'TestUser21', password: 'drowssap', email: 'test@example.com')
+      persisted_data = persisted_data(table: :users, id: user.id)
+      expect(user.id).to eq persisted_data['id']
+      expect(user.name).to eq('TestUser21')
+      expect(user.email).to eq 'test@example.com'
+    end
   end
 
-describe '.find' do
-  it 'finds a user by ID' do
-    user = User.create(name: 'TestUser21', password: 'drowssap', email: 'test@example.com')
-    result = User.find(user.id)
-    expect(result.id).to eq user.id
-    expect(result.email).to eq user.email
+  describe '.find' do
+    it 'finds a user by ID' do
+      user = User.create(name: 'TestUser21', password: 'drowssap', email: 'test@example.com')
+      result = User.find(user.id)
+      expect(result.id).to eq user.id
+      expect(result.email).to eq user.email
+    end
   end
-end
 
 end
--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -1,43 +1,52 @@
 require 'user'
 
-describe '.create' do
+describe User do
 
-  response = [{ "id" => '1', "name" => 'Test', "email" => 'test@example.com', "password" => 'drowsapp' }]
+  describe '.create' do
 
-  it 'creates a new user', :no_database_setup do
-    allow(DatabaseConnection).to receive(:query).and_return(response)
-    user = User.create(name: 'Test', email: 'test@example.com', password: 'drowsapp')
-    expect(user.id).to eq('1')
-    expect(user.name).to eq('Test')
-    expect(user.email).to eq('test@example.com')
+    response = [
+      { 
+        "id" => '1', "name" => 'Test', 
+        "email" => 'test@example.com', "password" => 'drowsapp' 
+      }
+    ]
+
+    it 'creates a new user', :no_database_setup do
+      allow(DatabaseConnection).to receive(:query).and_return(response)
+      user = User.create(name: 'Test', email: 'test@example.com', password: 'drowsapp')
+      expect(user.id).to eq('1')
+      expect(user.name).to eq('Test')
+      expect(user.email).to eq('test@example.com')
+    end
+
+    it 'hashes password using BCcrypt', :no_database_setup do
+      allow(DatabaseConnection).to receive(:query).and_return(response)
+      expect(BCrypt::Password).to receive(:create).with('drowsapp')
+      User.create(name: 'Test', email: 'test@example.com', password: 'drowsapp')
+    end
   end
 
-  it 'hashes password using BCcrypt', :no_database_setup do
-    allow(DatabaseConnection).to receive(:query).and_return(response)
-    expect(BCrypt::Password).to receive(:create).with('drowsapp')
-    User.create(name: 'Test', email: 'test@example.com', password: 'drowsapp')
-  end
-end
+  describe '.find' do
 
-describe '.find' do
-  result = [
-    { 
-      "id" => "10", 
-      "name" => "Test User", 
-      "email" => "email@example.com",
-    }
-  ]
+    result = [
+      { 
+        "id" => "10", 
+        "name" => "Test User", 
+        "email" => "email@example.com",
+      }
+    ]
 
-  it 'returns user based on id', :no_database_setup do
-    allow(DatabaseConnection).to receive(:query).and_return(result)
-    user = User.find("10")
-    expect(user.id).to eq("10")
-    expect(user.name).to eq("Test User")
-    expect(user.email).to eq("email@example.com")
-  end
+    it 'returns user based on id', :no_database_setup do
+      allow(DatabaseConnection).to receive(:query).and_return(result)
+      user = User.find("10")
+      expect(user.id).to eq("10")
+      expect(user.name).to eq("Test User")
+      expect(user.email).to eq("email@example.com")
+    end
 
-  it 'returns nil if no id passed', :no_database_setup do
-    expect(User.find(nil)).to eq nil
+    it 'returns nil if no id passed', :no_database_setup do
+      expect(User.find(nil)).to eq nil
+    end
   end
 
 end
